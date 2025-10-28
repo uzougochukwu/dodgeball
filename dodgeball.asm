b431392ba5e84e98c26904d9916175a8ad8ea5d5
Uzo Ugochukwu <uugochukwu@aol.com>
2025-10-28 20:35:24 +0000

    framerate fine but nintendo logo disappeared

--- dodgeball.asm
+++ dodgeball.asm
@@ -19,6 +19,14 @@ WaitForvBlank:
 	cp 144			; 44 - 153 is the vBlank period
 	jp c, WaitForvBlank	; cp automatically compares the accumulator value with 144, by subtracting 144 from a. if carry, it means that a - 144 was negative and set the carry bit, which means a was less than 144, so we go back to the start of the loop. if the carry bit is not set, then a is greater than or equal to 144, so we are in vBlank
 
+	; turn off LCD screen
+	ld a, 0
+	ld [rLCDC], a		; load 0 into the LCD control register to turn it off
+
+
+	; turn on LCD screen
+	ld a, LCDCF_ON | LCDCF_BGON ; bitwise OR of LCD on and LCD background on flags
+	ld [rLCDC], a		    ; load that into the LCD control register to turn it on
 
 
 End:
