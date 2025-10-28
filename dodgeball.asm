14cdf1c85f279c09a7a5295a1afd2092d718651a
Uzo Ugochukwu <uugochukwu@aol.com>
2025-10-28 19:44:41 +0000

    WaitForvBlank routine created

--- dodgeball.asm
+++ dodgeball.asm
@@ -13,6 +13,13 @@ SECTION "Header", ROM0[$100]
 
 EntryPoint:
 
+WaitForvBlank:
+	; wait until vBlank, then draw player controlled character	
+	ld a, [rLY]		; move the value of the Y coordinate of the vertical scanline into the accumulator
+	cp a, 144		; 144 - 153 is the vBlank period
+	jp z, Main			; if zero flag set, accumulator equals 144, so we jump to main and now we can do all of our work in the vBlank period
+
+Main:
 	
 
 
