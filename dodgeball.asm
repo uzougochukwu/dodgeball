0ebe95baefd3dec439c37dde74f1739f9a589e48
Uzo Ugochukwu <uugochukwu@aol.com>
2025-10-31 07:33:20 +0000

    ball bounces but not far enough

--- dodgeball.asm
+++ dodgeball.asm
@@ -373,7 +373,15 @@ HitOpponent:
 	; and we set the PlayerBallThrown flag to 0
 	ld a, 0
 	ld [PlayerBallThrown], a
+	call BounceOffOpponent
 
+	ret
+
+
+BounceOffOpponent:
+	ld a, [_OAMRAM + 4]	; y coord of ball is in a
+	add a, 4		; we want the ball to move down screen after hitting opponent
+	ld [_OAMRAM + 4], a
 	ret
 
 	; create a function BallThrownMovement that moves ball depending on who threw it and whether the ball has hit a wall yet, or player
